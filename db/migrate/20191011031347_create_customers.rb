2e4f0c430588fba0748350c0e6243db30375ec96
VBTX <jv.svedaite@gmail.com>
2019-10-10 22:20:10 -0500

    add not null and indexes to the schema and migrated

--- db/migrate/20191011031347_create_customers.rb
+++ db/migrate/20191011031347_create_customers.rb
@@ -1,12 +1,14 @@
 class CreateCustomers < ActiveRecord::Migration[5.2]
   def change
     create_table :customers do |t|
-      t.string :first_name
-      t.string :last_name
-      t.string :email
-      t.string :username
+      t.string :first_name , null: false
+      t.string :last_name , null: false
+      t.string :email , null: false
+      t.string :username , null: false
 
-      t.timestamps
+      t.timestamps		 null: false
     end
+    add_index :customers, :email, unique: true 
+    add_index :customers, :username, unique: true
   end
 end
