c740527a1c6577af6f65d3a51687e10f708de229
VBTX <jv.svedaite@gmail.com>
2019-10-10 21:58:46 -0500

    add email constraints via postgres, so that only emails that are accepted are gmail, also dropped regular rails schema and created sql schema

---
+++ db/migrate/20191011020954_add_email_constraint_to_users.rb
@@ -0,0 +1,20 @@
+class AddEmailConstraintToUsers < ActiveRecord::Migration[5.2]
+  def up
+  	execute %{
+  		ALTER TABLE 
+  		users 
+  		ADD CONSTRAINT 
+  		email_must_be_company_email
+  		CHECK (email ~* '^[^@]+@gmail\\.com$')
+  	}
+  end
+
+  def down
+  	execute %{
+  		ALTER TABLE 
+  		users 
+  		DROP CONSTRAINT
+  		email_must_be_company_email
+  	}
+  end
+end
